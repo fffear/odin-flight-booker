7a6a6e42c1b64ee60cd9c2f698e01c0e32e216fd
Kevin Vuong <c_k.k@hotmail.com>
2019-09-22 16:13:54 +1000

    fix sendgrid password typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENGRID_PASSWORD'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
